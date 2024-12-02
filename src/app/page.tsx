5c67a830ad09660b91d25d60ec696af7b68d2022
Saad Sayyed <saadsyed950@gmail.com>
2024-12-02 20:52:33 +0530

    Working on Contact

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -156,9 +156,6 @@ export default function Page() {
         <div className="grid items-center justify-center gap-4 px-4 text-center md:px-6 w-full py-12">
           <BlurFade delay={BLUR_FADE_DELAY * 16}>
             <div className="space-y-3">
-              <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
-                Contact
-              </div>
               <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
                 Get in Touch
               </h2>
